7e149afcae1488e1c0f6e8557cb4c931f072c1a8
kadetigor <kadet.igor3216@gmail.com>
2025-03-28 18:06:04 +0100

    changed git ignore

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const settings_1 = require("./settings");
 const app_1 = require("./app");
-app_1.app.listen(settings_1.SETTINGS.PORT, () => {
-    console.log(`Example app listening on port ${settings_1.SETTINGS.PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app_1.app.listen(settings_1.SETTINGS.PORT, () => {
+        console.log(`Example app listening on port ${settings_1.SETTINGS.PORT}`);
+    });
+}
+exports.default = app_1.app;
